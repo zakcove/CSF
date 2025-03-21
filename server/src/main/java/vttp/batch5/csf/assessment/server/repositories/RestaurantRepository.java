e5131cf9a01fbb893fa5eaf777eb5e3ac22b7ef0
zakcove <muhdzakir@gmail.com>
2025-03-21 15:58:13 +0800

    CSFv1.4

--- server/src/main/java/vttp/batch5/csf/assessment/server/repositories/RestaurantRepository.java
+++ server/src/main/java/vttp/batch5/csf/assessment/server/repositories/RestaurantRepository.java
@@ -20,10 +20,10 @@ public class RestaurantRepository {
     private static final String GET_MENU_ITEMS = 
         "SELECT * FROM menu_items ORDER BY name ASC";
 
-    private static final String SQL_VALIDATE_USER = 
+    private static final String VALIDATE_USER = 
         "select count(*) from customers where username = ? and password = sha2(?, 224)";
 
-    private static final String SQL_INSERT_ORDER = 
+    private static final String INSERT_ORDER = 
         "insert into place_orders (order_id, payment_id, order_date, total, username) values (?, ?, ?, ?, ?)";
 
     // TODO: Task 2.2
@@ -45,12 +45,12 @@ public class RestaurantRepository {
     }
 
     public boolean validateUser(String username, String password) {
-        Integer count = jdbcTemplate.queryForObject(SQL_VALIDATE_USER, Integer.class, username, password);
+        Integer count = jdbcTemplate.queryForObject(VALIDATE_USER, Integer.class, username, password);
         return count != null && count > 0;
     }
 
     public void saveOrder(String orderId, String paymentId, String username, double total) {
-        jdbcTemplate.update(SQL_INSERT_ORDER, 
+        jdbcTemplate.update(INSERT_ORDER, 
             orderId, paymentId, new Date(), total, username);
     }
 }
