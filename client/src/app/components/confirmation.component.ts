c6e0c558f930fb5d71f53e5c06107a84c29fea89
zakcove <muhdzakir@gmail.com>
2025-03-21 16:28:19 +0800

    CSFv1.4

--- client/src/app/components/confirmation.component.ts
+++ client/src/app/components/confirmation.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-confirmation',
@@ -6,8 +7,30 @@ import { Component } from '@angular/core';
   templateUrl: './confirmation.component.html',
   styleUrl: './confirmation.component.css'
 })
-export class ConfirmationComponent {
+export class ConfirmationComponent implements OnInit {
 
-  // TODO: Task 5
+  orderId: string = '';
+  paymentId: string = '';
+  total: number = 0;
+  date: Date = new Date();
 
+  constructor(private router: Router) {
+    const navigation = this.router.getCurrentNavigation();
+    if (navigation?.extras.state) {
+      this.orderId = navigation.extras.state['orderId'];
+      this.paymentId = navigation.extras.state['paymentId'];
+      this.total = navigation.extras.state['total'];
+      this.date = new Date();
+    }
+  }
+
+  ngOnInit(): void {
+    if (!this.orderId || !this.paymentId) {
+      this.router.navigate(['/']);
+    }
+  }
+
+  backToMenu() {
+    this.router.navigate(['/']);
+  }
 }
